539837f158f3ad3cb1d1bde0eec1af67de270473
Seb Glazebrook <me@sebglazebrook.com>
2014-06-24 20:43:39 +1000

    Fixed up syntax error in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ ROOT_DIR = Pathname.new(File.expand_path('../..', __FILE__))
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
 RSpec.configure do |config|
-  config.color_enabled = true
+  config.color = true
   config.formatter     = 'documentation'
 end
 

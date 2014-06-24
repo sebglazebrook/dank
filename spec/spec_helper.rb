f9ca83a5a7d3085a2cc37ca65adeccb0f68a9bb8
Seb Glazebrook <me@sebglazebrook.com>
2014-06-24 20:31:20 +1000

    Rspec installed

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require_relative '../lib/dank'
+
+ROOT_DIR = Pathname.new(File.expand_path('../..', __FILE__))
+
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.formatter     = 'documentation'
+end
+
+
+

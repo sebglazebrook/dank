c168abf49e0497ac777aa745630c0044844e7481
Seb Glazebrook <me@sebglazebrook.com>
2014-07-14 20:11:37 +1000

    Move interal methods to private

--- lib/dank/evaluators/expression_evaluator.rb
+++ lib/dank/evaluators/expression_evaluator.rb
@@ -1,19 +1,6 @@
 module Evaluators
   class ExpressionEvaluator
 
-    def expression_regex_lookup
-      {
-        'any_letters' => /[a-zA-Z]*/,
-        'any_lowercase_letters' => /[a-z]*/,
-        'any_uppercase_letters' => /[A-Z]*/,
-        'any_numbers' => /\d*/,
-        'at_least_one_number' => /\d+/,
-        'at_least_one_letter' => /[a-zA-Z]+/,
-        'at_least_one_lowercase_letter' => /[a-z]+/,
-        'at_least_one_uppercase_letter' => /[A-Z]+/,
-      }
-    end
-
     def evaluate expression
       if expression_regex_lookup[expression]
         Dank::Models::Expression.new(regex: expression_regex_lookup[expression], expression: expression)
@@ -22,5 +9,19 @@ module Evaluators
       end
     end
 
+    private
+
+    def expression_regex_lookup
+      {
+          'any_letters' => /[a-zA-Z]*/,
+          'any_lowercase_letters' => /[a-z]*/,
+          'any_uppercase_letters' => /[A-Z]*/,
+          'any_numbers' => /\d*/,
+          'at_least_one_number' => /\d+/,
+          'at_least_one_letter' => /[a-zA-Z]+/,
+          'at_least_one_lowercase_letter' => /[a-z]+/,
+          'at_least_one_uppercase_letter' => /[A-Z]+/,
+      }
+    end
   end
 end
--- lib/dank/evaluators/regex_evaluator.rb
+++ lib/dank/evaluators/regex_evaluator.rb
@@ -1,19 +1,6 @@
 module Evaluators
   class RegexEvaluator
-
-    def expression_regex_lookup
-      {
-        'any_letters' => /[a-zA-Z]*/,
-        'any_lowercase_letters' => /[a-z]*/,
-        'any_uppercase_letters' => /[A-Z]*/,
-        'any_numbers' => /\d*/,
-        'at_least_one_number' => /\d+/,
-        'at_least_one_letter' => /[a-zA-Z]+/,
-        'at_least_one_lowercase_letter' => /[a-z]+/,
-        'at_least_one_uppercase_letter' => /[A-Z]+/,
-      }
-    end
-
+    
     def evaluate regex
       if expression_regex_lookup.invert[regex]
         Dank::Models::Expression.new(regex: regex, expression: expression_regex_lookup.invert[regex])
@@ -21,5 +8,21 @@ module Evaluators
         raise Dank::Exceptions::UnknownRegex.new
       end
     end
+
+    private
+
+    def expression_regex_lookup
+      {
+          'any_letters' => /[a-zA-Z]*/,
+          'any_lowercase_letters' => /[a-z]*/,
+          'any_uppercase_letters' => /[A-Z]*/,
+          'any_numbers' => /\d*/,
+          'at_least_one_number' => /\d+/,
+          'at_least_one_letter' => /[a-zA-Z]+/,
+          'at_least_one_lowercase_letter' => /[a-z]+/,
+          'at_least_one_uppercase_letter' => /[A-Z]+/,
+      }
+    end
+
   end
 end
